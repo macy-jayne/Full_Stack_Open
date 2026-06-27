729098d58052872b363f0a8a2c24d334c244e4a9
macy-august <m-august@onu.edu>
2026-06-27 15:34:37 -0400

    Exercises 2.11 (Phonebook Step 6 - JSON Server Axios)

--- part2/Exercises2.6-2.11/src/App.jsx
+++ part2/Exercises2.6-2.11/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddNewEntryForm from './Components/AddNewEntryForm'
 import Filter from './Components/Filter'
 import Persons from './Components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filterBy, setFilterBy] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   return (
     <div>

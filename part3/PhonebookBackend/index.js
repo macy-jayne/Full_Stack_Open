5e432e320f2aa7ec737964e74844b1896ff8a204
macy-august <m-august@onu.edu>
2026-07-07 20:44:06 -0400

    small change

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('cors')
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)

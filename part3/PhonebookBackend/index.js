8b607592c59f094aff9e3c3993f59c12781ef724
macy-august <m-august@onu.edu>
2026-07-12 20:56:50 -0400

    forgot to delete some old code

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -90,7 +90,7 @@ app.post('/api/persons', (request, response) => {
 // The name or number is missing
 // The name already exists in the phonebook
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/PhonebookBackend/mongo.js
+++ part3/PhonebookBackend/mongo.js
@@ -19,28 +19,3 @@ const personSchema = new mongoose.Schema({
 })
 
 const Person = mongoose.model('Person', personSchema)
-
-if (process.argv.length === 3) {
-  Person.find({  }).then(result => {
-    console.log('Phonebook:')
-    result.forEach(person => {
-      console.log(person.name, '->', person.number)
-    })
-    mongoose.connection.close()
-  })
-} else if (process.argv.length === 5) {
-  const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4],
-  })
-
-  person.save().then(result => {
-    console.log('person added to phonebook!')
-    mongoose.connection.close()
-  })
-} else {
-    console.log('incorrect number of arguments')
-    mongoose.connection.close()
-}
-
-

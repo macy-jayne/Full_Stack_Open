bae27aad0c762a9345807d16891d5b25b4851ad5
macy-august <m-august@onu.edu>
2026-07-23 21:59:04 -0400

    exercise 4.2

---
+++ part4/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./Controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
